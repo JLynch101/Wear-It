7551add395a3799f4f6b4475021f90058b0da1f5
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-03-04 18:10:39 +0000

    Splash screen added

--- WearIt/app/src/main/java/ie/wit/wearit/SplashScreenActivity.kt
+++ WearIt/app/src/main/java/ie/wit/wearit/SplashScreenActivity.kt
@@ -1,11 +1,22 @@
 package ie.wit.wearit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+    @Suppress("DEPRECATION")
+    Handler().postDelayed(
+        {
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        },
+        2000
+    )
     }
 }
