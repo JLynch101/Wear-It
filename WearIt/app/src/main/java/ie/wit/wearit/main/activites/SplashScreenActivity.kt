829a819d6571b6d1f33942cd7b7d3b859fcd38e4
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-03-11 22:15:03 +0000

    bhujip

--- WearIt/app/src/main/java/ie/wit/wearit/main/activites/SplashScreenActivity.kt
+++ WearIt/app/src/main/java/ie/wit/wearit/main/activites/SplashScreenActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import ie.wit.wearit.R
+import androidx.activity.R
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
