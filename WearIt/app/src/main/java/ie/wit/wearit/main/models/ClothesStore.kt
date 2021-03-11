178d0cabe95114e7b8c0d747911031fbe1cd2567
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-03-11 22:31:00 +0000

    Removed old project

--- WearIt/app/src/main/java/ie/wit/wearit/main/models/ClothesStore.kt
+++
@@ -1,9 +0,0 @@
-package org.wit.clothes.models
-
-interface ClothesStore {
-    fun findAll(): List<ClothesModel>
-    fun create(clothes: ClothesModel)
-    fun update(clothes: ClothesModel)
-    fun delete(clothes: ClothesModel)
-}
-
