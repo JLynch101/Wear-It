ab41a3fd12e434821fa4102607537d582cf83635
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-03-11 23:49:08 +0000

    Fixed Fragments

---
+++ Wear it App/app/src/main/java/ie/wit/wearit/main/ClothesListActivity.kt
@@ -0,0 +1,67 @@
+package org.wit.clothes.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ie.wit.wearit.R
+import ie.wit.wearit.main.*
+import ie.wit.wearit.main.models.ClothesModel
+import kotlinx.android.synthetic.main.activity_clothes_list.*
+import kotlinx.android.synthetic.main.card_clothes.view.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.startActivityForResult
+
+
+class ClothesListActivity : AppCompatActivity(), ClothesListener {
+
+    lateinit var app: MainApp
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_clothes_list)
+        app = application as MainApp
+        toolbar.title = title
+        setSupportActionBar(toolbar)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = ClothesAdapter(app.clothess.findAll(), this)
+        loadClothess()
+    }
+
+    private fun loadClothess() {
+        showClothess( app.clothess.findAll())
+    }
+
+    fun showClothess (clothess: List<ClothesModel>) {
+        recyclerView.adapter = ClothesAdapter(clothess, this)
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_add -> startActivityForResult<ClothesActivity>(200)
+            R.id.item_map -> startActivity<ClothesMapsActivity>()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onClothesClick(clothes: ClothesModel) {
+        startActivityForResult(intentFor<ClothesActivity>().putExtra("clothes_edit", clothes), 0)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        loadClothess()
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+}
+
--- Wear it App/app/src/main/java/ie/wit/wearit/main/fragments/HomeFragment.kt
+++ Wear it App/app/src/main/java/ie/wit/wearit/main/fragments/HomeFragment.kt
@@ -1,92 +1,61 @@
 package ie.wit.wearit.main.fragments
 
-
-import android.content.Intent
 import android.os.Bundle
-import android.view.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import ie.wit.wearit.R
-import ie.wit.wearit.main.ClothesAdapter
-import ie.wit.wearit.main.ClothesListener
-import ie.wit.wearit.main.ClothesMapsActivity
-import ie.wit.wearit.main.MainApp
-import ie.wit.wearit.main.models.ClothesModel
-import kotlinx.android.synthetic.main.fragment_home.*
-import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.startActivityForResult
 
-
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    class ClothesListActivity : AppCompatActivity(), ClothesListener {
-
-        lateinit var app: MainApp
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.fragment_home)
-            app = application as MainApp
-            toolbar.title = title
-            setSupportActionBar(toolbar)
-
-            val layoutManager = LinearLayoutManager(this)
-            recyclerView.layoutManager = layoutManager
-            recyclerView.adapter = ClothesAdapter(app.clothess.findAll(), this)
-            loadClothess()
-        }
-
-        private fun loadClothess() {
-            showClothess(app.clothess.findAll())
-        }
-
-        fun showClothess(clothess: List<ClothesModel>) {
-            recyclerView.adapter = ClothesAdapter(clothess, this)
-            recyclerView.adapter?.notifyDataSetChanged()
-        }
-
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            menuInflater.inflate(R.menu.menu_main, menu)
-            return super.onCreateOptionsMenu(menu)
-        }
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-            when (item.itemId) {
-                R.id.item_add -> startActivityForResult<AddFragment.ClothesActivity>(200)
-                R.id.item_map -> startActivity<ClothesMapsActivity>()
-            }
-            return super.onOptionsItemSelected(item)
-        }
-
-        override fun onClothesClick(clothes: ClothesModel) {
-            startActivityForResult(
-                intentFor<AddFragment.ClothesActivity>().putExtra(
-                    "clothes_edit",
-                    clothes
-                ), 0
-            )
-        }
-
-        override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            loadClothess()
-            super.onActivityResult(requestCode, resultCode, data)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
         }
     }
 
-    fun newInstance(param1: String, param2: String) =
-        HomeFragment().apply {
-            arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_clothes_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HomeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
             }
-        }
+    }
 }
 
