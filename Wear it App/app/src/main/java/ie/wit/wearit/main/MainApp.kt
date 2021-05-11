dc65d526ab47042a871f31bd7467b437659cc445
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-05-11 23:17:18 +0100

    Updates

--- Wear it App/app/src/main/java/ie/wit/wearit/main/MainApp.kt
+++ Wear it App/app/src/main/java/ie/wit/wearit/main/MainApp.kt
@@ -1,6 +1,7 @@
 package ie.wit.wearit.main
 
 import android.app.Application
+import com.google.firebase.database.DatabaseReference
 import ie.wit.wearit.main.models.ClothesJSONStore
 import ie.wit.wearit.main.models.ClothesStore
 import org.jetbrains.anko.AnkoLogger
@@ -10,6 +11,7 @@ import org.jetbrains.anko.info
 class MainApp : Application(), AnkoLogger {
 
     lateinit var clothess: ClothesStore
+    lateinit var database: DatabaseReference
 
     override fun onCreate() {
         super.onCreate()
