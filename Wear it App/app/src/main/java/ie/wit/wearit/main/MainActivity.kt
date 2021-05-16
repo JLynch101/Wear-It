3122bd14732e0a9372e90cb60f4943330c8c9e34
Joe Lynch <48054458+JLynch101@users.noreply.github.com>
2021-05-16 21:55:34 +0100

    Fixed and Updated Nav Drawer

--- Wear it App/app/src/main/java/ie/wit/wearit/main/MainActivity.kt
+++ Wear it App/app/src/main/java/ie/wit/wearit/main/MainActivity.kt
@@ -5,54 +5,49 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import ie.wit.wearit.R
+import ie.wit.wearit.main.LoginActivity
+import ie.wit.wearit.main.MainApp
 import ie.wit.wearit.main.fragments.AddFragment
 import ie.wit.wearit.main.fragments.HomeFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.nav_header.view.*
+import org.jetbrains.anko.startActivity
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle: ActionBarDrawerToggle
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        app = application as MainApp
+        app.auth = FirebaseAuth.getInstance()
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        navView.getHeaderView(0).nav_header_email.text = app.auth.currentUser?.email
+
 
         navView.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.miItem1 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Item 1",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.miItem2 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Item 2",
-                    Toast.LENGTH_SHORT
-                ).show()
-                R.id.miItem3 -> Toast.makeText(
-                    applicationContext,
-                    "Clicked Item 3",
-                    Toast.LENGTH_SHORT
-                ).show()
+                R.id.miItem1 -> makeCurrentFragment(homeFragment)
+                R.id.miItem2 ->  makeCurrentFragment(addFragment)
+                R.id.miItem3 ->  signOut()
             }
             true
         }
 
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-        val homeFragment = HomeFragment()
-        val addFragment = AddFragment()
-
 
         makeCurrentFragment(homeFragment)
 
@@ -63,6 +58,12 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+    private fun signOut()
+    {
+        app.auth.signOut()
+        startActivity<LoginActivity>()
+        finish()
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)) {
@@ -80,3 +81,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+
+
